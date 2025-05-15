e74794918befa3d5c6f3555e9f8202ba127c89d3
Jonathan Fuller <fullerjn@gmail.com>
2025-05-15 15:44:44 -0500

    Changed frequency counter to count repeated bytes instead of strings

--- frequencyCounter.go
+++ frequencyCounter.go
@@ -2,29 +2,24 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 )
 
-func countLetterFrequencies(file io.Reader) (map[string]int, error) {
-	letterFreqs := map[string]int{}
+func countLetterFrequencies(file io.Reader) map[byte]int {
+	letterFreqs := map[byte]int{}
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		for _, b := range line {
-			if v, ok := letterFreqs[string(b)]; !ok {
-				letterFreqs[string(b)] = 1
-			} else {
-				letterFreqs[string(b)] = v + 1
-			}
+	reader := bufio.NewReader(file)
+	for {
+		b, err := reader.ReadByte()
+		if err != nil {
+			break
+		}
+		if _, ok := letterFreqs[b]; !ok {
+			letterFreqs[b] = 1
+		} else {
+			letterFreqs[b]++
 		}
 	}
 
-	if err := scanner.Err(); err != nil {
-		return map[string]int{}, fmt.Errorf("Scanner error: %w", err)
-	}
-
-	return letterFreqs, nil
+	return letterFreqs
 }
--- frequencyCounter_test.go
+++ frequencyCounter_test.go
@@ -9,38 +9,38 @@ func TestCountLetterFrequencies(t *testing.T) {
 	cases := []struct {
 		name           string
 		text           string
-		expectedChars  []string
+		expectedChars  []byte
 		expectedCounts []int
 	}{
 		{
 			name:           "simple string",
 			text:           "aaabbccaaabbcc",
-			expectedChars:  []string{"a", "b", "c"},
+			expectedChars:  []byte{'a', 'b', 'c'},
 			expectedCounts: []int{6, 4, 4},
 		}, {
 			name:           "lots of space",
 			text:           "       x    x   ",
-			expectedChars:  []string{" ", "x"},
+			expectedChars:  []byte{' ', 'x'},
 			expectedCounts: []int{14, 2},
 		},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			freqs, _ := countLetterFrequencies(strings.NewReader(tc.text))
+			freqs := countLetterFrequencies(strings.NewReader(tc.text))
 
 			assertFreqs(freqs, tc.expectedChars, tc.expectedCounts, t)
 		})
 	}
 }
 
-func assertFreqs(freqs map[string]int, expectedChars []string, expectedCounts []int, t *testing.T) {
+func assertFreqs(freqs map[byte]int, expectedChars []byte, expectedCounts []int, t *testing.T) {
 	t.Helper()
-	for i, char := range expectedChars {
-		if count, ok := freqs[char]; !ok {
-			t.Errorf("expected char missing from frequency counts: %s", char)
+	for i, b := range expectedChars {
+		if count, ok := freqs[b]; !ok {
+			t.Errorf("expected char missing from frequency counts: %b", b)
 		} else if count != expectedCounts[i] {
-			t.Errorf("expected char %s count to be %d, got %d", char, expectedCounts[i], count)
+			t.Errorf("expected char %b count to be %d, got %d", b, expectedCounts[i], count)
 		}
 	}
 }
